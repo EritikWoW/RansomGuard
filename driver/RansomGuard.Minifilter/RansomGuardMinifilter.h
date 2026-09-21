767013c18007afa18daa026e2a322889aa7eedef
EritikWoW <116751610+EritikWoW@users.noreply.github.com>
2026-09-21 04:19:02 +0300

    Declare pre-create minifilter callback

--- driver/RansomGuard.Minifilter/RansomGuardMinifilter.h
+++ driver/RansomGuard.Minifilter/RansomGuardMinifilter.h
@@ -16,6 +16,8 @@ DRIVER_INITIALIZE DriverEntry;
 NTSTATUS RgUnload(_In_ FLT_FILTER_UNLOAD_FLAGS Flags);
 NTSTATUS RgInstanceSetup(_In_ PCFLT_RELATED_OBJECTS FltObjects, _In_ FLT_INSTANCE_SETUP_FLAGS Flags,
                          _In_ DEVICE_TYPE VolumeDeviceType, _In_ FLT_FILESYSTEM_TYPE VolumeFilesystemType);
+FLT_PREOP_CALLBACK_STATUS RgPreCreate(_Inout_ PFLT_CALLBACK_DATA Data, _In_ PCFLT_RELATED_OBJECTS FltObjects,
+                                      _Flt_CompletionContext_Outptr_ PVOID *CompletionContext);
 FLT_PREOP_CALLBACK_STATUS RgPreWrite(_Inout_ PFLT_CALLBACK_DATA Data, _In_ PCFLT_RELATED_OBJECTS FltObjects,
                                      _Flt_CompletionContext_Outptr_ PVOID *CompletionContext);
 FLT_PREOP_CALLBACK_STATUS RgPreSetInformation(_Inout_ PFLT_CALLBACK_DATA Data, _In_ PCFLT_RELATED_OBJECTS FltObjects,

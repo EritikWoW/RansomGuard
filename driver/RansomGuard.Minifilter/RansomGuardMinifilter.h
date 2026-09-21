faaea86bc46e1a1a971bdb1ef6f3640b2c1d8225
EritikWoW <116751610+EritikWoW@users.noreply.github.com>
2026-09-21 04:59:35 +0300

    Declare post-create reconciliation context

--- driver/RansomGuard.Minifilter/RansomGuardMinifilter.h
+++ driver/RansomGuard.Minifilter/RansomGuardMinifilter.h
@@ -12,12 +12,19 @@ typedef struct _RG_WORK_ITEM {
     RG_EVENT Event;
 } RG_WORK_ITEM, *PRG_WORK_ITEM;
 
+typedef struct _RG_CREATE_CONTEXT {
+    PFLT_FILE_NAME_INFORMATION PreCreateNameInfo;
+    ULONGLONG RequestSequence;
+} RG_CREATE_CONTEXT, *PRG_CREATE_CONTEXT;
+
 DRIVER_INITIALIZE DriverEntry;
 NTSTATUS RgUnload(_In_ FLT_FILTER_UNLOAD_FLAGS Flags);
 NTSTATUS RgInstanceSetup(_In_ PCFLT_RELATED_OBJECTS FltObjects, _In_ FLT_INSTANCE_SETUP_FLAGS Flags,
                          _In_ DEVICE_TYPE VolumeDeviceType, _In_ FLT_FILESYSTEM_TYPE VolumeFilesystemType);
 FLT_PREOP_CALLBACK_STATUS RgPreCreate(_Inout_ PFLT_CALLBACK_DATA Data, _In_ PCFLT_RELATED_OBJECTS FltObjects,
                                       _Flt_CompletionContext_Outptr_ PVOID *CompletionContext);
+FLT_POSTOP_CALLBACK_STATUS RgPostCreate(_Inout_ PFLT_CALLBACK_DATA Data, _In_ PCFLT_RELATED_OBJECTS FltObjects,
+                                        _In_opt_ PVOID CompletionContext, _In_ FLT_POST_OPERATION_FLAGS Flags);
 FLT_PREOP_CALLBACK_STATUS RgPreWrite(_Inout_ PFLT_CALLBACK_DATA Data, _In_ PCFLT_RELATED_OBJECTS FltObjects,
                                      _Flt_CompletionContext_Outptr_ PVOID *CompletionContext);
 FLT_PREOP_CALLBACK_STATUS RgPreSetInformation(_Inout_ PFLT_CALLBACK_DATA Data, _In_ PCFLT_RELATED_OBJECTS FltObjects,
